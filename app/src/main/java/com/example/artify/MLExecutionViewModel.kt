edff27e8dcbf3d74e6e7b406316367db8cd8aa02
yavuzozguven <yavuzozguven@hotmail.com>
2020-10-15 15:49:49 +0300

    first commit

---
+++ app/src/main/java/com/example/artify/MLExecutionViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.artify
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.content.Context
+import android.graphics.Bitmap
+import android.media.ExifInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class MLExecutionViewModel : ViewModel() {
+
+    private val _styledBitmap = MutableLiveData<ModelExecutionResult>()
+
+    val styledBitmap: LiveData<ModelExecutionResult>
+        get() = _styledBitmap
+
+    private val viewModelJob = Job()
+    private val viewModelScope = CoroutineScope(viewModelJob)
+
+    fun onApplyStyle(
+        context: Context,
+        contentFilePath: String,
+        styleFilePath: String,
+        styleTransferModelExecutor: StyleTransferModelExecutor,
+        inferenceThread: ExecutorCoroutineDispatcher,
+        exif: ExifInterface,
+        bitmap: Bitmap,
+        bitmap_style: Bitmap?
+    ) {
+        viewModelScope.launch(inferenceThread) {
+            val result =
+                styleTransferModelExecutor.execute(contentFilePath, styleFilePath, context,exif,bitmap,bitmap_style)
+            _styledBitmap.postValue(result)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/artify/SplashScreen.kt
@@ -0,0 +1,51 @@
+package com.example.artify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
+class SplashScreen : AppCompatActivity() {
+
+    var anim: Animation? = null
+    var logo: TextView? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+
+
+
+
+
+
+        //animation
+        anim = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+
+        //hooks
+        logo = findViewById(R.id.logo)
+
+        logo?.animation = anim
+
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            finish()
+        }, 1500)
+    }
+}
